af8967666578c47434e1265805de4825a1465079
Maksim Pecherskiy <max@maksimize.com>
2015-05-02 21:45:25 -0700

    Passing wk4!

--- wk4/rankall.R
+++ wk4/rankall.R
@@ -6,67 +6,60 @@ rankall <- function(outcomeArg, rankArg) {
                            "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", 
                            "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", 
                            "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")]
-  
-  osplit = split(outcomes, outcomes$State)
+  colnames(outcomes) <- c("hospital", "state", "heart attack", "heart failure", "pneumonia")
   
   ## Get targetCol based on outcomeArg
   targetCol <- getOutcomeCol(outcomeArg)
   
-  ## Filter our the NAs from the column we're looking at.
-  sh_clean <- states_hospitals[!is.na(states_hospitals[targetCol]), ]
+  if (!is.numeric(rankArg)) 
+    rankArg <- tolower(rankArg)
   
+  ## Subset outcomes list only to what's relevant.
+  ## Cut out nonrelevant columns, with only the outcome.
+  outcomes <- outcomes[, c("hospital", "state", targetCol)]
+  ## Cut out all NAs for that outcome.
+  outcomes <- outcomes[complete.cases(outcomes), ]
   
-  ## Set Ranks
-  sh_clean$rank = 1:nrow(sh_clean)
+  ## Split the hospital list by state.
+  osplit = split(outcomes, outcomes$state)
   
-  ## Order Alphabetically and By Rank
-  sh_clean <- sh_clean[order(sh_clean[targetCol], sh_clean$Hospital.Name),]
-  
-  ## Set Ranks
-  sh_clean$rank = 1:nrow(sh_clean)
-  
-
-  
-  best_hospital_name <- as.character(best_hospital$Hospital.Name)
-  if (length(best_hospital_name) == 0)
-    NA
-  else 
-    best_hospital_name
+  os <- sapply(osplit, rankHospitalsInState, targetCol, rankArg)
+  os <- data.frame(state = names(os), hospital = os)
+  os
   
 }
 
-getRankArg <- function(rankArg) {
-  #todo -lowercase
+
+rankHospitalsInState <- function(stateHospitals, targetCol, rankArg){
+  ## Order Alphabetically and By Rank
+  sh <- stateHospitals[order(stateHospitals[targetCol], stateHospitals$hospital), ]
+  ## Set Rank
+  sh$rank = 1:nrow(sh)
+  hospitalRow <- getHospitalAtRank(sh, targetCol, rankArg)
+  hname <- as.character(hospitalRow$hospital)
+  if (length(hname) == 0)
+    NA
+  else 
+    hname
+}
+
+
+getHospitalAtRank <- function(sh, targetCol, rankArg) {
   if (is.numeric(rankArg)) 
-    best_hospital <- sh_clean[sh_clean$rank == rankArg, ]
+    best_hospital <- sh[sh$rank == rankArg, ]
   else if (rankArg == "worst")
-    best_hospital <- sh_clean[which.max(sh_clean[[targetCol]]), ]
+    best_hospital <- sh[which.max(sh[[targetCol]]), ]
   else if (rankArg == "best")
-    best_hospital <- sh_clean[which.min(sh_clean[[targetCol]]), ]
+    best_hospital <- sh[which.min(sh[[targetCol]]), ]
 }
 
 getOutcomeCol <- function(outcomeArg) {
   outcomeArg <- tolower(outcomeArg)
+  
   ## Validate Outcome.
   possible_outcomes <- c("heart attack", "heart failure", "pneumonia")
   if (length(which(possible_outcomes == outcomeArg)) == 0)  
     stop("invalid outcome")
   
-  ## Get Outcome ColName
-  targetCol = "Hospital.30.Day.Death..Mortality..Rates.from."
-  if (outcomeArg == "heart attack")
-    targetCol = paste(targetCol, "Heart.Attack", sep = "")
-  if (outcomeArg == "heart failure")
-    targetCol = paste(targetCol, "Heart.Failure", sep = "")
-  if (outcomeArg == "pneumonia")
-    targetCol = paste(targetCol, "Pneumonia", sep = "")
-  targetCol
+  outcomeArg
 }
-
-## Subset by state
-#states_hospitals = osplit[[state]]
-
-## Validate state.
-#if (is.null(nrow(states_hospitals)))  
-#  stop("invalid state")
-
